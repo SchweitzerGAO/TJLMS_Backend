aae97c5ba6e447d49d7d9d38d1660f75450c39bd
Charles Gao <1030587461@qq.com>
2022-01-03 13:28:35 +0800

    我不想再写后端了

--- tjlms/src/main/java/edu/tongji/tjlms/model/SummatorResultEntityPK.java
+++ tjlms/src/main/java/edu/tongji/tjlms/model/SummatorResultEntityPK.java
@@ -1,14 +1,11 @@
 package edu.tongji.tjlms.model;
 
-import edu.tongji.tjlms.dto.SummatorResultDto;
-
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Id;
+import java.io.Serializable;
 import java.util.Objects;
 
-@Entity
-@Table(name = "summator_result", schema = "lms", catalog = "")
-@IdClass(SummatorResultEntityPK.class)
-public class SummatorResultEntity {
+public class SummatorResultEntityPK implements Serializable {
     private String stuId;
     private Integer b1;
     private Integer b2;
@@ -20,12 +17,8 @@ public class SummatorResultEntity {
     private Integer s4;
     private Integer c;
 
-    public SummatorResultEntity() {
-
-    }
-
-    @Id
     @Column(name = "stu_id", nullable = false, length = 45)
+    @Id
     public String getStuId() {
         return stuId;
     }
@@ -34,85 +27,78 @@ public class SummatorResultEntity {
         this.stuId = stuId;
     }
 
-    @Id
     @Column(name = "B1", nullable = false)
+    @Id
     public Integer getB1() {
         return b1;
     }
-
 
     public void setB1(Integer b1) {
         this.b1 = b1;
     }
 
-    @Id
     @Column(name = "B2", nullable = false)
+    @Id
     public Integer getB2() {
         return b2;
     }
-
 
     public void setB2(Integer b2) {
         this.b2 = b2;
     }
 
-    @Id
     @Column(name = "B3", nullable = false)
+    @Id
     public Integer getB3() {
         return b3;
     }
-
 
     public void setB3(Integer b3) {
         this.b3 = b3;
     }
 
-    @Id
     @Column(name = "B4", nullable = false)
+    @Id
     public Integer getB4() {
         return b4;
     }
-
 
     public void setB4(Integer b4) {
         this.b4 = b4;
     }
 
-    @Id
     @Column(name = "S1", nullable = false)
+    @Id
     public Integer getS1() {
         return s1;
     }
-
 
     public void setS1(Integer s1) {
         this.s1 = s1;
     }
 
-    @Id
     @Column(name = "S2", nullable = false)
+    @Id
     public Integer getS2() {
         return s2;
     }
-
 
     public void setS2(Integer s2) {
         this.s2 = s2;
     }
 
-    @Id
     @Column(name = "S3", nullable = false)
+    @Id
     public Integer getS3() {
         return s3;
     }
-
 
     public void setS3(Integer s3) {
         this.s3 = s3;
     }
 
-    @Id
     @Column(name = "S4", nullable = false)
+    @Id
     public Integer getS4() {
         return s4;
     }
@@ -121,12 +107,11 @@ public class SummatorResultEntity {
         this.s4 = s4;
     }
 
-    @Id
     @Column(name = "C", nullable = false)
+    @Id
     public Integer getC() {
         return c;
     }
-
 
     public void setC(Integer c) {
         this.c = c;
@@ -136,7 +121,7 @@ public class SummatorResultEntity {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        SummatorResultEntity that = (SummatorResultEntity) o;
+        SummatorResultEntityPK that = (SummatorResultEntityPK) o;
         return Objects.equals(stuId, that.stuId) && Objects.equals(b1, that.b1) && Objects.equals(b2, that.b2) && Objects.equals(b3, that.b3) && Objects.equals(b4, that.b4) && Objects.equals(s1, that.s1) && Objects.equals(s2, that.s2) && Objects.equals(s3, that.s3) && Objects.equals(s4, that.s4) && Objects.equals(c, that.c);
     }
 
@@ -144,18 +129,4 @@ public class SummatorResultEntity {
     public int hashCode() {
         return Objects.hash(stuId, b1, b2, b3, b4, s1, s2, s3, s4, c);
     }
-
-    public SummatorResultEntity(SummatorResultDto srd)
-    {
-        this.b1 = srd.getB1();
-        this.b2 = srd.getB2();
-        this.b3 = srd.getB3();
-        this.b4 = srd.getB4();
-        this.s1 = srd.getS1();
-        this.s2 = srd.getS2();
-        this.s3 = srd.getS3();
-        this.s4 = srd.getS4();
-        this.c = srd.getC();
-    }
-
 }
--- tjlms/src/main/java/edu/tongji/tjlms/repository/SummatorResultRepository.java
+++ tjlms/src/main/java/edu/tongji/tjlms/repository/SummatorResultRepository.java
@@ -1,11 +1,12 @@
 package edu.tongji.tjlms.repository;
 
 import edu.tongji.tjlms.model.SummatorResultEntity;
+import edu.tongji.tjlms.model.SummatorResultEntityPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface SummatorResultRepository extends JpaRepository<SummatorResultEntity,String> {
+public interface SummatorResultRepository extends JpaRepository<SummatorResultEntity, SummatorResultEntityPK> {
     List<SummatorResultEntity> findAllByStuId(String id);
 
 }
